f5575307ae2c0373c489dcc60889e0871d076f7e
majacar <mayaplavusa@yahoo.com>
2017-02-15 15:34:34 +0100

    Post text or image on my wall

--- routes/posts.js
+++ routes/posts.js
@@ -123,3 +123,98 @@ module.exports.post = function (req, res, next) {
     return next(error);
   }
 };
+
+/*
+ * @api {post} /post
+ * @apiVersion 1.0.0
+ * @apiDescription Post text or photo on my wall
+ * @apiGroup Post
+ *
+ * @apiHeader {String} Token authorization value.
+ * @apiHeaderExample {json} Token Example:
+ * {"Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJ..."}
+ *
+ * @apiSuccessExample Success-Response:
+   HTTP/1.1 201 OK
+{
+  "status": "ok",
+  "results": {
+    "__v": 0,
+    "updatedAt": "2017-02-08T19:44:53.338Z",
+    "createdAt": "2017-02-08T19:44:53.338Z",
+    "text": "Hello!!!",
+    "_id": "589b7535bfe2f61bb730ecae"
+  }
+}
+}
+*/
+
+
+module.exports.post = function (req, res, next) {
+  if (req.body && req.user._id) {
+  
+            var post = new Post();
+            var updateSet = { $addToSet: { wall: post._id }};
+
+            function updateUser(user_id, updateSet, cb) {
+              User.findOneAndUpdate({ _id: req.user._id }, updateSet, { new: true }, function (err, savedUser) {
+                if (err) {
+                  var error = new Error();
+                  error.name = 'MongoSaveError';
+                  error.message = err.message;
+                  return cb(error, null);
+                } else {
+                  return cb(null, savedUser);
+                }
+              });
+            }
+
+            if (!_.isUndefined(req.body.text)) {
+              post.text = req.body.text;
+
+              post.save(function (err, data) {
+                    if (err) {
+                      var error = new Error();
+                      error.name = 'MongoSaveError';
+                      error.message = err.message;
+                      return next(error);
+                    } else {
+                  updateUser(req.user._id, updateSet, function (err, data) {
+                  if (err) return next(err);
+                      res.status(201).send({
+                            status: 'ok',
+                            results: post
+                          });
+                    });
+                     }
+                  });
+            } else {
+              if (!_.isUndefined(req.body.image) && !_.isUndefined(req.body.image.type) && !_.isUndefined(req.body.image.image)) {
+                utils.resizeUploadImage(req.body.image, config.maxImageSize(), config.amazonS3().bucket, function (err, url) {
+                  if (err) return next(err);
+                  post.image = url;
+                  post.save(function (err, data) {
+                    if (err) {
+                      var error = new Error();
+                      error.name = 'MongoSaveError';
+                      error.message = err.message;
+                      return next(error);
+                    } else {
+                       updateUser(req.user._id, updateSet, function (err, data) {
+                  if (err) return next(err);
+                      res.status(201).send({
+                            status: 'ok',
+                            results: post
+                          });
+                    });
+                     }
+                  });
+                });
+              }
+            }
+  } else {
+    var error = new Error();
+    error.name = 'MissingParamsError';
+    return next(error);
+  }
+};
