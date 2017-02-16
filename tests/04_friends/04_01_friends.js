11c82b50487e1c04c526714fc3e076e93eb3e3db
majacar <mayaplavusa@yahoo.com>
2017-02-16 21:03:45 +0100

    Add tests for view my friends and other users friends

--- tests/04_friends/04_01_friends.js
+++ tests/04_friends/04_01_friends.js
@@ -134,4 +134,47 @@ describe('Send friend request and Add user to Friends', function () {
     });
   });
 
+   it('GET /friends Should return my friends', function (done) {
+    userHelper.register_user(function (result) {
+
+      request(app)
+        .get('/api/v1/friends')
+        .set('Accept', 'application/json')
+        .set('Authorization', 'Bearer ' + result.token)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+
+          should.not.exist(err);
+          should.exist(res.body.results);
+          done();
+        });
+    });
+  });
+
+    it('GET /friends Should return other user friends', function (done) {
+    userHelper.register_user(function (result1) {
+
+      userHelper.register_user(function (result2) {
+
+      request(app)
+        .get('/api/v1/friends/' + result2.user._id)
+        .set('Accept', 'application/json')
+        .set('Authorization', 'Bearer ' + result1.token)
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            throw err;
+          }
+
+          should.not.exist(err);
+          should.exist(res.body.results);
+          done();
+        });
+      });
+    });
+  });
+
 });
